e0a11212cd8e8963e3ac29688b0a7e9a13998294
wyzheng <wyzheng1997@163.com>
2024-08-28 15:34:05 +0800

    refactor: 通过macro扩展进行重构，并添加IDE提示

---
+++ .ide_helper.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Illuminate\Database\Eloquent {
+    /**
+     * @method $this search(array $options = [])
+     * @method $this sort(array $options = [])
+     */
+    class Builder {}
+
+    /**
+     * @method static Builder search(array $options = [])
+     * @method static Builder sort(array $options = [])
+     */
+    class Model {}
+}
---
+++ src/ServiceProvider.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Ugly\SearchModel;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    public function register(): void
+    {
+        /**
+         * @param  array  $options  查询配置
+         *                          1、['name' => 'like'] PS: name like '%xxx%'
+         *                          2、['category_id' => ['=', 'cate_id']]   PS: category_id = request('cate_id')
+         *                          3、['type' => fn($query, $input) => $query->where('type', $input)] PS: 自定义查询
+         *                          5、['admin.name' => 'like'] PS: whereHas('admin', fn($q) => $->where('name', 'like', '%xxx%')) PS: 关联查询
+         * @param  array  $relations  关联查询
+         */
+        Builder::macro('search', function (array $options = [], array $relations = []) {
+            /** @var Builder $builder */
+            $builder = $this;
+            if (blank($options)) {
+                return $builder;
+            }
+            if (! blank($relations)) {
+                $builder->with($relations);
+            }
+
+            return $builder->where(function ($query) use ($options) {
+                // 获取请求参数
+                foreach ($options as $field => $operator) {
+                    $fieldInfo = pathinfo($field);
+                    $isWhereHas = isset($fieldInfo['extension']);
+
+                    // whereHas 默认key PS: 'admin.name' 转换成 'admin_name'
+                    if ($isWhereHas) {
+                        $field = str_replace('.', '_', $field);
+                    }
+
+                    // 自定义 key 优先级最高
+                    $field = is_array($operator) ? $operator[1] : $field;
+
+                    // 获取请求参数
+                    $input = request($field);
+
+                    // 跳过空值
+                    if (blank($input)) {
+                        continue;
+                    }
+
+                    // 自定义查询
+                    if ($operator instanceof \Closure) {
+                        $query->where(fn ($q) => call_user_func($operator, $q, $input));
+
+                        continue; // 进入下一个查询参数
+                    }
+
+                    // 操作符号
+                    $operator = is_array($operator) ? $operator[0] : $operator; // =, >, >=, <,<=, like, in, between
+                    switch ($operator) {
+                        case 'in':
+                        case 'between':
+                            $method = 'where'.ucfirst($operator);
+                            $args = [is_array($input) ? $input : explode(',', $input)];
+                            break;
+                        case 'like':
+                            $method = 'where';
+                            $args = ['like', '%'.$input.'%'];
+                            break;
+                        default:
+                            $method = 'where';
+                            $args = [$operator, $input];
+                            break;
+                    }
+
+                    if ($isWhereHas) {
+                        $query->whereHas($fieldInfo['filename'], fn ($q) => $q->$method($fieldInfo['extension'], ...$args));
+                    } else {
+                        $query->$method($fieldInfo['filename'], ...$args);
+                    }
+                }
+            });
+        });
+
+        /**
+         * 排序.
+         *
+         * @param  array  $options  排序配置, 支持闭包
+         *                          [
+         *                          'name',
+         *                          'price' => fn($query, $direction) => $query->orderBy('total_price', $direction)
+         *                          ]
+         */
+        Builder::macro('sort', function (array $options = []) {
+            /** @var Builder $builder */
+            $builder = $this;
+            $sort_by = (string) request('sort_by');
+            if (! blank($sort_by)) {
+                //请求参数格式：sort_by=asc(last_modified),desc(email)
+                preg_match_all('/(asc|desc)\((.*?)\)/', $sort_by, $matches);
+                foreach ($matches[0] as $index => $match) {
+                    $direction = $matches[1][$index];
+                    $field = $matches[2][$index];
+                    if (in_array($field, $options)) {
+                        $builder->orderBy($field, $direction);
+                    } elseif (data_get($options, $field) instanceof \Closure) {
+                        call_user_func($options[$field], $builder, $direction);
+                    }
+                }
+            }
+
+            return $builder;
+        });
+    }
+}
